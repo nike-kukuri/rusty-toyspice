ae94b0996b3cffccacc398d4bf21b573a8d59be2
nikekukuri <nikekukuri0327@gmail.com>
2023-02-22 12:55:38 +0900

    mainのtestを整理（GitHubコミット確認を兼ねる）

--- src/main.rs
+++ src/main.rs
@@ -85,27 +85,7 @@ fn main() {
 mod tests {
     use super::*;
 
-    /*
     #[test]
     fn netlist() {
-        let mut v = HashMap::new();
-        let mut r = HashMap::new();
-        let mut c = HashMap::new();
-        let mut l = HashMap::new();
-
-        v.insert("v1".to_string(), Element{ pos: 1, neg: 2, value: 3.0 });
-        r.insert("r1".to_string(), Element{ pos: 2, neg: 3, value: 0.3 });
-        l.insert("l1".to_string(), Element{ pos: 3, neg: 4, value: 1.0e-9 });
-        c.insert("c1".to_string(), Element{ pos: 4, neg: 1, value: 3.0e-6 });
-        c.insert("c2".to_string(), Element{ pos: 4, neg: 1, value: 3.0e-6 });
-        let netlist = Netlist { v, r, c, l };
-
-        assert_eq!(vec!["v1"], netlist.v.keys().collect::<Vec<_>>());
-        assert_eq!(vec!["r1"], netlist.r.keys().collect::<Vec<_>>());
-        assert_eq!(vec!["l1"], netlist.l.keys().collect::<Vec<_>>());
-        assert_eq!(vec!["c1", "c2"], netlist.c.keys().collect::<Vec<_>>());
-
-        netlist.v.values().into_iter().for_each(|x| println!("first elem = {}, seconde elem = {}" , x.pos, x.neg));
     }
-    */
 }
